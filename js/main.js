dc288cfdce90aee234687fd54072db87c3d57f93
unknown <cimicimicilung4868@gmail.com>
2024-05-18 13:42:26 +0700

    test revan

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu');
+const hamburgerMenu = document.querySelector('hamburger-menu');
+const iconBar = document.querySelector('.icon-bar');
+const iconClose = document.querySelector('.icon-close');
+
+function displayMenu() {
+    if(menu.classList.contains('tampil')) {
+        menu.classList.remove('tampil');
+        iconBar.style.display='inline';
+        iconClose.style.display='none';
+    } else {
+        menu.classList.add('tampil');
+        iconBar.style.display ='none';
+        iconClose.style.display ='inline';
+    }
+}
